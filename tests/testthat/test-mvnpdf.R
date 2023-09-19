15ee862ffefd8708c608fb3a4fe01c968891ca69
Robin Genuer <robin.genuer@u-bordeaux.fr>
2023-09-19 11:51:18 +0200

    update testthat folder with Wickham & Bryan convention

--- tests/testthat/test-mvnpdf.R
+++ tests/testthat/test-mvnpdf.R
@@ -1,3 +1,11 @@
+test_that("correct result for univariate gaussian", {
+  expect_equal(mvnpdf(x=matrix(1.96), mean=0, varcovM=diag(1), Log=FALSE)$y,
+               dnorm(1.96))
+  expect_equal(mvnpdf(x=matrix(c(1.96, -0.5), ncol = 2), mean=0,
+                      varcovM=diag(1), Log=FALSE)$y, dnorm(c(1.96, -0.5)))
+})
+
+
 test_that("correct results for bivariate gaussian", {
   expect_equal(mvnpdf(x=matrix(rep(1.96,2), nrow=2, ncol=1),
                       mean=c(0, 0), varcovM=diag(2), Log=FALSE)$y,
@@ -12,6 +20,6 @@ test_that("correct results for bivariate gaussian", {
                            mean=c(0, 0), varcovM=diag(2), Log=FALSE)$y,
                mvtnorm::dmvnorm(rep(1.96, 2)))
   expect_equal(mvnpdfsmartC(x=matrix(rep(1.96,2), nrow=2, ncol=1),
-                      mean=c(0, 0), varcovM=diag(2), Log=FALSE)[1,1],
+                            mean=c(0, 0), varcovM=diag(2), Log=FALSE)[1,1],
                mvtnorm::dmvnorm(rep(1.96, 2)))
 })
--- tests/testthat/test-univariate.R
+++
@@ -1,6 +0,0 @@
-test_that("correct result for univariate gaussian", {
-  expect_equal(mvnpdf(x=matrix(1.96), mean=0, varcovM=diag(1), Log=FALSE)$y,
-               dnorm(1.96))
-  expect_equal(mvnpdf(x=matrix(c(1.96, -0.5), ncol = 2), mean=0, varcovM=diag(1), Log=FALSE)$y,
-               dnorm(c(1.96, -0.5)))
-})
