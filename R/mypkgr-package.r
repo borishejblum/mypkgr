792d7391b076d627f1cad2cc88e0be2d00bbffa8
borishejblum <boris.hejblum@gmail.com>
2023-09-22 11:02:14 +0200

    update roxygen version and GHA

--- R/mypkgr-package.r
+++ R/mypkgr-package.r
@@ -7,9 +7,8 @@
 #' What are the most important functions...
 #'
 #' @name mypkgr
-#' @docType package
 #'
 #' @useDynLib mypkgr, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #'
-NULL
+"_PACKAGE"
